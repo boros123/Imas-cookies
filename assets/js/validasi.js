0c5f5459250f4c2f9fd50e9347f3296b0a009eaf
boros123 <nauvaall.qa@gmail.com>
2024-06-06 23:24:55 +0700

    nambah projek

---
+++ assets/js/validasi.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#form').on('submit', function(e){
+    e.preventDefault(); 
+  
+    var name = $('#name').val();
+    var phone = $('#phone').val();
+    var date = $('#tgl').val();
+    var jumlah = $('#jumlah').val();
+    var jenis = $('#jenis-kue').val();
+    var alamat = $('#alamat').val();
+    
+    var currentTime = new Date();
+    var realtime = currentTime.toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' });
+    var message = "Nama Pembeli: " + name + "%0A" +
+                  "Telepon: " + phone + "%0A" +
+                  "Order Tanggal: " + realtime + "%0A" +
+                  "Untuk Tanggal: " + date + "%0A" +
+                  "Jumlah: " + jumlah + " Bungkus" + "%0A" +
+                  "Jenis Kue: " + jenis + "%0A" +
+                  "Alamat: " + alamat;
+
+   
+    var whatsappURL = "https://api.whatsapp.com/send?phone=62895377073012&text=" + message;
+    window.open(whatsappURL, '_blank');
+  });
+});
