31a3ec955e368943db9a86f1c49b997ce5a9bd1b
boros123 <nauvaall.qa@gmail.com>
2024-06-07 11:41:20 +0700

    ganti no valid

--- assets/js/validasi.js
+++ assets/js/validasi.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
                   "Jenis Kue: " + jenis + "%0A" +
                   "Alamat: " + alamat;
 
-   
+  //  ganti no hp
     var whatsappURL = "https://api.whatsapp.com/send?phone=62895377073012&text=" + message;
     window.open(whatsappURL, '_blank');
   });
